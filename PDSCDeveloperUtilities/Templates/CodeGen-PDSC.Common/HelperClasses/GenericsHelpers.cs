3c332db652698c11487c932720b4a9df98fd8df8
Paul D. Sheriff <psheriff@pdsa.com>
2023-11-09 10:09:48 -0600

    PDSC Developer Utilities V9.0.1

--- PDSCDeveloperUtilities/Templates/CodeGen-PDSC.Common/HelperClasses/GenericsHelpers.cs
+++ PDSCDeveloperUtilities/Templates/CodeGen-PDSC.Common/HelperClasses/GenericsHelpers.cs
@@ -5,13 +5,28 @@ public static class GenericsHelpers
   public static T ChangeType<T>(object? o)
   {
     Type conversionType = Nullable.GetUnderlyingType(typeof(T)) ?? typeof(T);
-    if (o == null) {
+    if (conversionType.Name == "Guid") {
+      string? value = Convert.ToString(o);
+      if (!string.IsNullOrEmpty(value) && Guid.TryParse(value, out Guid tmp)) {
+        tmp = Guid.Parse(value);
+        tmp = tmp.Equals(Guid.Empty) ? Guid.NewGuid() : tmp;
+        return (T)Convert.ChangeType(tmp, conversionType);
+      }
+      else {
 #pragma warning disable CS8603 // Possible null reference return.
-      return default;
+        return (T)Convert.ChangeType(Guid.NewGuid(), conversionType);
 #pragma warning restore CS8603 // Possible null reference return.
+      }
     }
     else {
-      return (T)Convert.ChangeType(o, conversionType);
+      if (o == null) {
+#pragma warning disable CS8603 // Possible null reference return.
+        return default;
+#pragma warning restore CS8603 // Possible null reference return.
+      }
+      else {
+        return (T)Convert.ChangeType(o, conversionType);
+      }
     }
   }
 }
