0811cf8ca37786219ddeabe4e52ec44db069c504
Paul D. Sheriff <psheriff@pdsa.com>
2023-12-23 10:16:39 -0600

    PDSC Developer Utilities 9.1.0

--- PDSCDeveloperUtilities/Templates/CodeGen-PDSC.Common/BaseClasses/SettingsBase.cs
+++ PDSCDeveloperUtilities/Templates/CodeGen-PDSC.Common/BaseClasses/SettingsBase.cs
@@ -1,64 +1,24 @@
-﻿//#nullable disable
-
-using Microsoft.Extensions.Configuration;
+﻿using PDSC.Common.JWT;
 
 namespace PDSC.Common;
 
 /// <summary>
-/// This class reads those common properties that 
-/// These are normally read in from the appsettings.json file of your application.
+/// This class holds the common properties that are normally 
+/// read in from the appsettings.json file of your application.
 /// </summary>
 public class SettingsBase : CommonBase
 {
-  #region Constructor
-  public SettingsBase() : base()
-  {
-  }
-
-  /// <summary>
-  /// Constructor for ApplicationSettings class
-  /// </summary>
-  public SettingsBase(IConfiguration config) : base()
-  {
-    Configuration = config;
-  }
-  #endregion
-
   #region Private Variables
-  private IConfiguration? _Configuration;
-  private string _DefaultConnectionString = string.Empty;
-  private string _ApplicationName = string.Empty;
-  private int _RecordsPerPage;
-  private string _LogFileName = string.Empty;
-  private string _SiteUrl = string.Empty;
+  private string _ApplicationName = "Your Application Name Here";
+  private string _BaseWebAPIService = "https://localhost:3000";
+  private string _InfoMessageDefault = "Problem Attempting to {Verb} using the {ClassName} API. Please Contact Your System Administrator.";
+  private DateTime _LoginDateTime = DateTime.Now;
+  private string _CORSPolicyName = "CORSYourApplicationName";
+  private int _RecordsPerPage = 10;
   #endregion
 
   #region Public Properties
-  /// <summary>
-  /// Get/Set Configuration Reader
-  /// </summary>
-  public IConfiguration? Configuration
-  {
-    get { return _Configuration; }
-    set
-    {
-      _Configuration = value;
-      RaisePropertyChanged(nameof(Configuration));
-    }
-  }
-
-  /// <summary>
-  /// Get/Set the Default Connection String
-  /// </summary>
-  public string DefaultConnectionString
-  {
-    get { return _DefaultConnectionString; }
-    set
-    {
-      _DefaultConnectionString = value;
-      RaisePropertyChanged(nameof(DefaultConnectionString));
-    }
-  }
+  public JwtSettings JWTSettings { get; set; } = new();
 
   /// <summary>
   /// Get/Set the Application Name
@@ -74,6 +34,58 @@ public class SettingsBase : CommonBase
   }
 
   /// <summary>
+  /// Get/Set the Base Web API Service
+  /// </summary>
+  public string BaseWebAPIService
+  {
+    get { return _BaseWebAPIService; }
+    set
+    {
+      _BaseWebAPIService = value;
+      RaisePropertyChanged(nameof(BaseWebAPIService));
+    }
+  }
+
+  /// <summary>
+  /// Get/Set the Info Message Default
+  /// </summary>
+  public string InfoMessageDefault
+  {
+    get { return _InfoMessageDefault; }
+    set
+    {
+      _InfoMessageDefault = value;
+      RaisePropertyChanged(nameof(InfoMessageDefault));
+    }
+  }
+
+  /// <summary>
+  /// Get/Set the date/time logged in
+  /// </summary>
+  public DateTime LoginDateTime
+  {
+    get { return _LoginDateTime; }
+    set
+    {
+      _LoginDateTime = value;
+      RaisePropertyChanged(nameof(LoginDateTime));
+    }
+  }
+
+  /// <summary>
+  /// Get/Set the CORS Policy Name
+  /// </summary>
+  public string CORSPolicyName
+  {
+    get { return _CORSPolicyName; }
+    set
+    {
+      _CORSPolicyName = value;
+      RaisePropertyChanged(nameof(CORSPolicyName));
+    }
+  }
+
+  /// <summary>
   /// Get/Set RecordsPerPage
   /// </summary>
   public int RecordsPerPage
@@ -84,59 +96,6 @@ public class SettingsBase : CommonBase
       _RecordsPerPage = value;
       RaisePropertyChanged(nameof(RecordsPerPage));
     }
-  }
-
-  /// <summary>
-  /// Get/Set the log file name
-  /// </summary>
-  public string LogFileName
-  {
-    get { return _LogFileName; }
-    set
-    {
-      _LogFileName = value;
-      RaisePropertyChanged(nameof(LogFileName));
-    }
-  }
-
-  /// <summary>
-  /// Get/Set the site url
-  /// </summary>
-  public string SiteUrl
-  {
-    get { return _SiteUrl; }
-    set
-    {
-      _SiteUrl = value;
-      RaisePropertyChanged(nameof(SiteUrl));
-    }
-  }
-  #endregion
-
-  #region Init Method
-  /// <summary>
-  /// Initialize the Application Information
-  /// </summary>
-  public override void Init()
-  {
-    base.Init();
-
-    RecordsPerPage = 10;
-  }
-  #endregion
-
-  #region LoadApplicationSettings Method
-  /// <summary>
-  /// Override this method to read in the standard PDSC application settings, plus your own settings
-  /// </summary>
-  public virtual void LoadApplicationSettings()
-  {
-    // TODO: Load all standard settings
-    //DefaultConnectionString = Configuration.GetValue<string>("ConnectionStrings:DefaultConnection");
-    //ApplicationName = Configuration.GetValue<string>("SiteSettings:ApplicationName");
-    //RecordsPerPage = Configuration.GetValue<int>("SiteSettings:RecordsPerPage");
-    //SiteUrl = Configuration.GetValue<string>("SiteSettings:SiteUrl");
-    //LogFileName = Configuration.GetValue<string>("SiteSettings:LogFileName");
   }
   #endregion
 }
