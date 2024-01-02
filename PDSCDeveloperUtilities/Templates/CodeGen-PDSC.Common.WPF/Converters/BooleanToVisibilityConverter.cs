a106e96f1d5f05b66447df597caa1ef8dd475f7f
Paul D. Sheriff <psheriff@pdsa.com>
2024-01-02 14:32:33 -0600

    PDSC Developer Utilities 9.1.0

---
+++ PDSCDeveloperUtilities/Templates/CodeGen-PDSC.Common.WPF/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PDSC.Common.WPF.Converters;
+
+/// <summary>
+/// Call this converter to change a true value to Visible and a false value to Collapsed.
+/// </summary>
+public class BooleanToVisibilityConverter : IValueConverter
+{
+  /// <summary>
+  /// Convert a True/False value to Visibility.Visible/Visibility.Collapsed value
+  /// </summary>
+  /// <param name="value">A boolean value</param>
+  /// <param name="targetType">The type of object</param>
+  /// <param name="parameter">Any parameters passed via XAML</param>
+  /// <param name="culture">The current culture</param>
+  /// <returns>A Visibility Enumeration</returns>
+  public object Convert(object value, Type targetType,
+                        object parameter, CultureInfo culture)
+  {
+    if ((bool)value)
+      return Visibility.Visible;
+    else
+      return Visibility.Collapsed;
+  }
+
+  /// <summary>
+  /// NOT IMPLEMENTED
+  /// </summary>
+  /// <param name="value">A boolean value</param>
+  /// <param name="targetType">The type of object</param>
+  /// <param name="parameter">Any parameters passed via XAML</param>
+  /// <param name="culture">The current culture</param>
+  /// <returns>NOT IMPLEMENTED</returns>
+  public object ConvertBack(object value, Type targetType,
+                            object parameter, CultureInfo culture)
+  {
+    throw new NotImplementedException("BooleanToVisibility ConvertBack Method Not Implemented");
+  }
+}
---
+++ PDSCDeveloperUtilities/Templates/CodeGen-PDSC.Common.WPF/Converters/BooleanToVisibilityHiddenConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PDSC.Common.WPF.Converters;
+
+/// <summary>
+/// Call this converter to change a true value to Visible and a false value to Hidden.
+/// </summary>
+public class BooleanToVisibilityHiddenConverter : IValueConverter
+{
+  /// <summary>
+  /// Convert a True/False value to Visibility.Visible/Visibility.Hidden value
+  /// </summary>
+  /// <param name="value">A boolean value</param>
+  /// <param name="targetType">The type of object</param>
+  /// <param name="parameter">Any parameters passed via XAML</param>
+  /// <param name="culture">The current culture</param>
+  /// <returns>A Visibility Enumeration</returns>
+  public object Convert(object value, Type targetType,
+                        object parameter, CultureInfo culture)
+  {
+    if ((bool)value)
+      return Visibility.Visible;
+    else
+      return Visibility.Hidden;
+  }
+
+  /// <summary>
+  /// NOT IMPLEMENTED
+  /// </summary>
+  /// <param name="value">A boolean value</param>
+  /// <param name="targetType">The type of object</param>
+  /// <param name="parameter">Any parameters passed via XAML</param>
+  /// <param name="culture">The current culture</param>
+  /// <returns>NOT IMPLEMENTED</returns>
+  public object ConvertBack(object value, Type targetType,
+                            object parameter, CultureInfo culture)
+  {
+    throw new NotImplementedException("BooleanToVisibilityHiddenConverter ConvertBack Method Not Implemented");
+  }
+}
